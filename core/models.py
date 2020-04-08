b1bafbf54fa45a4ff23acc04eae1338cc5879ba6
Your Name <you@example.com>
2020-04-08 21:13:19 +0900

    #4.0 TimeStampedModel

---
+++ core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class TimeStampedModel(models.Model):
+
+    """ Time Stamped Model """
+
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
+
+    class Meta:
+        abstract = True
+
