9a50b8b276debca8d71ecab8c99d6759a73bacb4
Your Name <you@example.com>
2020-04-13 19:02:08 +0900

    #8.8 Intercepting Model save() and admin_save()

--- rooms/models.py
+++ rooms/models.py
@@ -92,9 +92,16 @@ class Room(core_models.TimeStampedModel):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        self.city = str.capitalize(self.city)
+        super().save(*args, **kwargs)
+
     def total_rating(self):
         all_reviews = self.reviews.all()
         all_ratings = 0
-        for review in all_reviews:
-            all_ratings += review.rating_average()
-        return all_ratings / len(all_reviews)
+        if len(all_reviews) == 0:
+            return 0
+        else:
+            for review in all_reviews:
+                all_ratings += review.rating_average()
+            return all_ratings / len(all_reviews)
