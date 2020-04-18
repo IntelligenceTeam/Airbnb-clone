ab1b9e196b268283eada26da22c8a9e7843eb5be
Your Name <you@example.com>
2020-04-18 20:34:37 +0900

    #11.2 Pages List Navigation

--- rooms/views.py
+++ rooms/views.py
@@ -1,17 +1,24 @@
+from math import ceil
 from django.shortcuts import render
 from . import models
 
 
 def all_rooms(request):
-    page = int(request.GET.get("page", 1))  # str타입이기 때문에 int형변환을 해줘야 한다.
+    page = request.GET.get("page", 1)
+    page = int(page or 1)  # ?page=에 값이 없을 때는 1로 들어가게 설정.
     page_size = 10
     limit = page_size * page
     offset = limit - page_size
     all_rooms = models.Room.objects.all()[offset:limit]
-    return render(request, "rooms/home.html", context={"rooms": all_rooms})
-
-    """
-        limit - 페이지 제한 수, 몇 개의 페이지를 보여줄 것인가
-        offset - 시작 페이지를 계산하기 위함. 1~10페이지까지 보여준다 할 때 시작페이지인 1을 가리키는 인덱스.
-
-    """
+    page_count = ceil(models.Room.objects.count() / page_size)
+    # django db에서 해당 테이블의 objects(데이터)의 갯수를 출력하는 count()를 사용.
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count),
+        },
+    )
