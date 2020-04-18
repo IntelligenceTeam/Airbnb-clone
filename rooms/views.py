b0ef9427ce3199c154a8b8a1379c6f6dca2676a8
Your Name <you@example.com>
2020-04-18 23:21:11 +0900

    #11.5 get_page vs page

--- rooms/views.py
+++ rooms/views.py
@@ -7,6 +7,6 @@ from . import models
 def all_rooms(request):
     page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", context={"rooms": rooms},)
+    paginator = Paginator(room_list, 10, orphans=5)  # orphan은 per_page보다 크지 않다.
+    rooms = paginator.page(int(page))
+    return render(request, "rooms/home.html", context={"page": rooms},)
