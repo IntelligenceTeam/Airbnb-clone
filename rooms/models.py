6a1785fa8edc667f85ef52f297612f35be216661
Your Name <you@example.com>
2020-04-19 23:59:01 +0900

    #12.1 get_absolute_url

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.urls import reverse
 from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as user_models
+
 
 # Create your models here.
 class AbstractItem(core_models.TimeStampedModel):
@@ -95,6 +96,9 @@ class Room(core_models.TimeStampedModel):
     def save(self, *args, **kwargs):
         self.city = str.capitalize(self.city)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("rooms:detail", kwargs={"pk": self.pk})
 
     def total_rating(self):
         all_reviews = self.reviews.all()
