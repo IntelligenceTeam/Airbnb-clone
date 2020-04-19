e69ec6f4d5c7a8566e103eeeacb0746b61af6041
Your Name <you@example.com>
2020-04-19 16:30:58 +0900

    #11.8 Class Based Views part 2

--- rooms/views.py
+++ rooms/views.py
@@ -10,5 +10,5 @@ class HomeView(ListView):
     paginate_by = 10
     paginate_orphans = 5
     ordering = "created"
+    context_object_name = "rooms"
 
-    # class based view 식으로 할 때는 render할 파일명이 애플리케이션 이름과 동일시하거나 오류가 날 때 알려준다
