09946f3a249e5cd47748211b5456c5957b8bba67
Your Name <you@example.com>
2020-04-20 23:35:36 +0900

    #12.3 Http404()

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "3u&fsugj!7z%!ln++1#kcl)kt%h8gb+-g3i2w))4nko(y%3tz)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = "*"
 
 
 # Application definition
