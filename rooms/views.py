58074be184a3550b21584b88a3449d4efb1395cf
Your Name <you@example.com>
2020-04-21 23:14:13 +0900

    #13.3 Amenities and Facilities Form

--- rooms/views.py
+++ rooms/views.py
@@ -27,10 +27,36 @@ def search(request):
     city = str.capitalize(city)
     country = request.GET.get("country", "KR")
     room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    s_amenities = request.GET.get("amenities")
+    s_facilities = request.GET.get("facilities")
+    print(s_amenities, s_facilities)
+
     room_types = models.RoomType.objects.all()
 
-    form = {"city": city, "s_room_type": room_type, "s_country": country}
+    form = {
+        "city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+    }
 
-    choices = {"countries": countries, "room_types": room_types}
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
 
-    return render(request, "rooms/search.html", {**form, **choices},)
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+
+    return render(request, "rooms/search.html", {**form, **choices})
