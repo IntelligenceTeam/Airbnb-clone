c8704553d5ededa1585627b49a5b1299398d927d
Your Name <you@example.com>
2020-04-22 22:54:11 +0900

    #13.8 I love Django Forms For Ever

--- rooms/forms.py
+++ rooms/forms.py
@@ -1,7 +1,23 @@
 from django import forms
+from django_countries.fields import CountryField
 from . import models
+
 
 class SearchForm(forms.Form):
     city = forms.CharField(initial="Anywhere")
+    country = CountryField(default="KR").formfield()
     price = forms.IntegerField(required=False)
-    room_type = forms.ModelChoiceField(queryset=models.RoomType.objects.all())
+    room_type = forms.ModelChoiceField(
+        required=False, empty_label="Any Kind", queryset=models.RoomType.objects.all()
+    )
+    guests = forms.IntegerField(required=False)
+    bedrooms = forms.IntegerField(required=False)
+    baths = forms.IntegerField(required=False)
+    instant_book = forms.BooleanField(required=False)
+    superhost = forms.BooleanField(required=False)
+    amenities = forms.ModelMultipleChoiceField(
+        queryset=models.Amenity.objects.all(), widget=forms.CheckboxSelectMultiple
+    )  # ModelMultipleChoiceField는 체크박스를 여러개 체크할 수 있는 필드이다.
+    facilities = forms.ModelChoiceField(
+        queryset=models.Facility.objects.all(), widget=forms.CheckboxSelectMultiple
+    )
