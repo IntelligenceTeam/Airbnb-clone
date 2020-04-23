4d0b121a860883f8e2df9e248dbe5a430b698f54
Your Name <you@example.com>
2020-04-23 16:14:53 +0900

    #14.2 Validating Email

--- users/forms.py
+++ users/forms.py
@@ -6,3 +6,16 @@ class LoginForm(forms.Form):
 
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean_email(self):
+        print("clean email")
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(username=email)
+            return email
+        except models.User.DoesNotExist:  # 해당 유저가 없을 경우
+            raise forms.ValidationError("User does not Exist")
+
+    def clean_password(self):
+        print("clean password")
+        return "lalalalala"
--- users/views.py
+++ users/views.py
@@ -5,9 +5,11 @@ from . import forms
 
 class LoginView(View):
     def get(self, request):
-        form = forms.LoginForm()
+        form = forms.LoginForm(initial={"email": "capunet@naver.com"})
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
-        print(form)
+        if form.is_valid():
+            print(form.cleaned_data)  # form을 정리해준 데이터
+        return render(request, "users/login.html", {"form": form})
