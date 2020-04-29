4243e0cc553cbb45482b636450417e91eb4ed736
Your Name <you@example.com>
2020-04-29 23:57:51 +0900

    #17.0 Github Login part 1

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+import os
 from django.views import View
 from django.views.generic import FormView
 from django.urls import reverse_lazy
@@ -55,3 +56,15 @@ def complete_verification(request, key):
         # to do : add error message
         pass
     return redirect(reverse("core:home"))  # 인증이 완료되면 홈으로 돌아가게끔 한다.
+
+
+def github_login(request):
+    client_id = os.environ.get("GH_ID")
+    redirect_uri = "http://localhost:8000/users/login/github/callback"
+    return redirect(
+        f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user"
+    )
+
+
+def github_callback(request):
+    pass
