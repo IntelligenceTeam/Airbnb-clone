b5b15789c491e804e6800b55b23fd4d8f03eefb9
Your Name <you@example.com>
2020-05-05 21:51:43 +0900

    #18.1 Finishing Kakao Login

--- users/views.py
+++ users/views.py
@@ -95,7 +95,6 @@ def github_callback(request):
                     },
                 )
                 profile_json = api_request.json()
-                print(profile_json)
                 username = profile_json.get("login", None)
                 if username is not None:
                     name = profile_json.get("name")
@@ -112,6 +111,7 @@ def github_callback(request):
                             username=email,
                             bio=bio,
                             login_method=models.User.LOGIN_GITHUB,
+                            email_verified=True,
                         )  # 그냥 create, password를 신경쓰지 않기 때문..
                         user.set_unusable_password()
                         user.save()
@@ -127,11 +127,11 @@ def github_callback(request):
 
 
 def kakao_login(request):
-    app_key = os.environ.get("KAKAO_ID")
+    client_id = os.environ.get("KAKAO_ID")
     redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
     return redirect(
-        f"https://kauth.kakao.com/oauth/authorize?client_id={app_key}&redirect_uri={redirect_uri}&response_type=code HTTP/1.1"
-    )
+        f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
+    )  # 끝에 HTTP/1.1 없애니까 오류 안 뜸.
 
 
 class KakaoException(Exception):
@@ -141,8 +141,45 @@ class KakaoException(Exception):
 def kakao_callback(request):
     try:
         code = request.GET.get("code")
+        client_id = os.environ.get("KAKAO_ID")
+        client_secret = os.environ.get("KAKAO_SECRET")
+        redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
         token_request = requests.get(
-            f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id"
+            f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}&client_secret={client_secret}"
         )
+        token_json = token_request.json()
+        error = token_json.get("error", None)
+        if error is not None:
+            raise KakaoException()
+        access_token = token_json.get("access_token")
+        profile_request = requests.get(
+            "https://kapi.kakao.com/v2/user/me",  # 2019/11/26 기준으로 v2로 바뀜. 강의댓글 참고
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
+        profile_json = profile_request.json()
+        email = profile_json.get("kakao_account").get("email", None)
+        if email is None:
+            raise KakaoException()
+        properties = profile_json.get("properties")
+        nickname = properties.get("nickname")
+        profile_image = (
+            profile_json.get("kakao_account").get("profile").get("profile_image_url")
+        )
+        try:
+            user = models.User.objects.get(email=email)
+            if user.login_method != models.User.LOGIN_KAKAO:
+                raise KakaoException()
+        except models.User.DoesNotExist:
+            user = models.User.objects.create(
+                email=email,
+                username=email,
+                first_name=nickname,
+                login_method=models.User.LOGIN_KAKAO,
+                email_verified=True,
+            )
+            user.set_unusable_password()
+            user.save()
+        login(request, user)
+        return redirect(reverse("core:home"))
     except KakaoException:
         return redirect(reverse("users:login"))
