2eaa02b6834bbdb8f968f8e25473a6fbf15b94bd
Your Name <you@example.com>
2020-05-10 20:00:21 +0900

    #20.5 Room Cards part 1

--- rooms/models.py
+++ rooms/models.py
@@ -109,3 +109,7 @@ class Room(core_models.TimeStampedModel):
             for review in all_reviews:
                 all_ratings += review.rating_average()
             return round(all_ratings / len(all_reviews), 2)
+
+    def first_photo(self):
+        (photo,) = self.photos.all()[:1]
+        return photo.file.url
