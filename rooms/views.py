aec9dbbab5bbc8c345780d5530f6b69d7af63088
Your Name <you@example.com>
2020-05-19 22:18:02 +0900

    #23.4 Edit Photo

--- rooms/views.py
+++ rooms/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect, reverse
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from users import mixins as user_mixins
 from . import models, forms
 
@@ -165,3 +166,22 @@ def delete_photo(request, room_pk, photo_pk):
         return redirect(reverse("rooms:photos", kwargs={"pk": room_pk}))
     except models.Room.DoesNotExist:
         return redirect(reverse("core:home"))
+
+
+class EditPhotoView(user_mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateView):
+
+    model = models.Photo
+    template_name = "rooms/photo_edit.html"
+    pk_url_kwarg = "photo_pk"
+    success_message = "Photo Updated"
+    fields = ("caption",)
+
+    def get_success_url(self):
+        room_pk = self.kwargs.get("room_pk")
+        print(room_pk)
+        return reverse("rooms:photos", kwargs={"pk": room_pk})
+
+    # UpdateView는 사진을 찾을 수 없다.
+    # url들에 pk가 없어서, room_pk, photo_pk라고 되어 있기 때문이다.
+    # UpdateView는 pk라는 값의 인자만 찾는다.
+    # 그걸 바꾸는 변수가 pk_url_kwarg이다.
