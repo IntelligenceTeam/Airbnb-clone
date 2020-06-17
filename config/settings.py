eb1af1b9d64af5622d37215bd6c0d8b26478dd98
Your Name <you@example.com>
2020-06-17 22:51:06 +0900

    #26.5 container commands

--- config/settings.py
+++ config/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET")
+SECRET_KEY = os.environ.get("DJANGO_SECRET", "C@NX29OxgjyylxTna$Nz")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".elasticbeanstalk.com"]
+ALLOWED_HOSTS = [
+    "airbnb-clone.eba-rja8a647.ap-northeast-2.elasticbeanstalk.com",
+    "localhost",
+]
 
 # Application definition
 
