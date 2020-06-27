3839bf7b70d9c0b0143c737313578e490342b1da
Your Name <you@example.com>
2020-06-27 22:57:36 +0900

    modify setting - sentry

--- config/settings.py
+++ config/settings.py
@@ -174,11 +174,10 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 
 
 # Sentry
-
 if not DEBUG:
 
-    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    DEFAULT_FILE_STORAGE = "config.custom_storages.UploadStorage"
+    STATICFILES_STORAGE = "config.custom_storages.StaticStorage"
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = "airbnb-clone-peakend"
@@ -187,6 +186,7 @@ if not DEBUG:
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/uploads/"
 
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
