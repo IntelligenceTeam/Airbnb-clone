bb0dd6004e2e157df237cc46b56cdc657069e20e
Your Name <you@example.com>
2020-06-28 22:44:35 +0900

    modify - djangoconfig, add aws s3 region and pipenv lock -r requirements

--- config/settings.py
+++ config/settings.py
@@ -186,7 +186,6 @@ if not DEBUG:
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/uploads/"
 
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
